455407b63983dbfd0c68316a0f2746160e349da0
Bogdan Buchkovskiy <89581425+Buchkovskiy1996@users.noreply.github.com>
2022-04-15 18:43:35 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+  
+  // Делаем функцию нажатие на кнопку меню появлание списка
+
+$('.menu__btn, .menu a').on('click', function(){
+  $('.menu__item').toggleClass('menu__item--active')
+});
+$('.menu__btn').on('click', function(){
+  $('.menu__btn').toggleClass('menu__btn--active')
+});
+
+// Делаем плавний скролл
+
+$(".menu a, .logo__footer").on("click", function (e) {
+  e.preventDefault();
+  var id  = $(this).attr('href'),
+    top = $(id).offset().top;
+  $('body,html').animate({scrollTop: top}, 1500);
+});
+  
+});
